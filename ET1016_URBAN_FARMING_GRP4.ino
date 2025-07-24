1e4ac9bd321be044b18eb5026df2ea45e0fd6e99
TWEJay <twejayden69.25@ichat.sp.edu.sg>
2025-07-24 15:43:52 +0800

    Add files via upload

---
+++ ET1016_URBAN_FARMING_GRP4.ino
@@ -0,0 +1,25 @@
+
+Title: URBAN FARMING
+Names: Jayden Toh, Jerren Lim
+Admission No's: 2517072, 2505875
+Class: DCEP/FT/1A/09
+Description: monitoring and control of temp and humidity (measuring the temp. and humidity of different zones for the crops)
+
+
+
+
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
