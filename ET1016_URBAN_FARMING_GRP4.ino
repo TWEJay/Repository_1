c69a41f349a3e7c27d7506d97c907af15d004e27
TWEJay <twejayden69.25@ichat.sp.edu.sg>
2025-08-13 01:20:26 +0800

    Update ET1016_URBAN_FARMING_GRP4.ino

--- ET1016_URBAN_FARMING_GRP4.ino
+++ ET1016_URBAN_FARMING_GRP4.ino
@@ -265,10 +265,77 @@ void handleButtonPress(void)
    if (mode < 5)
     mode = 7;
 
+   //insert entry point for temp humidity sensor here 
+    temp_main()
+
    while (digitalRead(BUTTONK2) == 0);
    delay(1500);
   }
 
+	
+
   return;
 }
+
+
+
+void main(void) {
+	displayChoices()//console displays instructions 
+    //handleButtonPress() //busy waiting for input 
+
+}
+void displayTemperature(int8_t temperature)
+{
+  int8_t temp[4];
+  if(temperature < 0)
+	{
+		temp[0] = INDEX_NEGATIVE_SIGN;
+		temperature = abs(temperature);
+	}
+	else if(temperature < 100)temp[0] = INDEX_BLANK;
+	else temp[0] = temperature/100;
+	temperature %= 100;
+	temp[1] = temperature / 10;
+	temp[2] = temperature % 10;
+	temp[3] = 12;	          //index of 'C' for celsius degree symbol.
+	disp.display(temp);
+  }
+
+  
+void displayHumidity(int8_t humi)
+{
+  int8_t temp[4];
+  if(humi < 100)temp[0] = INDEX_BLANK;
+  else temp[0] = humi/100;
+  humi %= 100;
+  temp[1] = humi / 10;
+  temp[2] = humi % 10;
+  temp[3] = 18;	          //index of 'H' for celsius degree symbol.
+  disp.display(temp);
+  
+}
+void temp_main() {
+  //handleButtonPress();
+  //do{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+    if (isnan(t) || isnan(h)) {
+    displayError();
+      } 
+  else{
+    displayTemperature((int8_t)t);//
+    delay(3000);
+    displayHumidity((int8_t)h);//
+    Serial.print(t);
+    Serial.print(",");
+    Serial.println(h);
+    delay(3000);
+  }
+}
+
+
+
+
+
+main()
 
